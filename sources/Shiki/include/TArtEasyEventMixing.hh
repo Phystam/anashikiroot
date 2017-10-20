4a4334994ec0c7d546cba140969ce527c281af2f
TomaiTakato <tomai@mail.nucl.ap.titech.ac.jp>
2017-10-20 17:00:03 +0900

    EasyEventMixing, need more bug fixes

--- sources/Shiki/include/TArtEasyEventMixing.hh
+++ sources/Shiki/include/TArtEasyEventMixing.hh
@@ -18,27 +18,44 @@ public:
   TArtEasyEventMixing();
   TArtEasyEventMixing(std::vector<TLorentzVector> part1, std::vector<TLorentzVector> part2,Double_t bins,Double_t Erelmax);
   void LoadData(std::vector<TLorentzVector> part1, std::vector<TLorentzVector> part2);
-  void SetErelMax(Double_t val){fErelmax=val;}
+  void ReconstructData();
+  void SetErelMax(Double_t val){fErelmax=val;finit_erelmax=true;}
+  void SetNbins(Double_t val){fnbins=val;finit_nbins=true;}
   Double_t Mix();
   void DoIteration();
   Int_t GetNumVirtualPairs();
   Double_t GetRelativeEnergy();
+  Double_t GetWeight();
   Int_t GetNumPairs(){return fneve;}
   Bool_t GetNextVirtualPair();
-  void CalcCorrelationFunction();
-
-private:
+  Bool_t GetNextPair();
+  Int_t GetCounterFrag(){return fcounter_frag;}
+  Int_t GetCounterNeut(){return fcounter_neut;}
+  void MakeCorrelatedSpectrum();
+  void MakeUncorrelatedSpectrum(Int_t i);
+  void CalcCorrelationFunction(Int_t i);
+  void MakeCorrelationMatrix(Int_t i);
+  void IterationOfMeanCorrelation();
+  void MakeInterpolation(TH1* hist, const Double_t ValueToCheck, Double_t &MeanValue, Double_t& MeanValueError);
   void InitCounter(){
     fcounter_frag=0;
     fcounter_neut=0;
+    fcounter=0;
   }
+  void ShowStatus(Int_t now, Int_t num,TString str);
+
+private:
   std::vector<TLorentzVector> ffragtable;
   std::vector<TLorentzVector> fneuttable;
   std::vector<Double_t> fCm_frag;
-  std::vector<Double_t> fCm_feut;
-  TH1* fhcorrfunc;
+  std::vector<Double_t> fCm_neut;
+  std::vector<Double_t> fweightmatrix;
+  std::vector<Double_t> ferrorweight;
+  std::vector<Int_t> fignored_frag; // ignored neutrons for each fragment
+  std::vector<Int_t> fignored_neut; // ignored fragments for each neutron
+  std::vector<TH1*> fhcorrfunc;
   TH1* fherel;
-  TH1* fhereluncor;
+  std::vector<TH1*> fhereluncor;
   Double_t fweight;
   Double_t ferror;
   Double_t fnbins;
@@ -46,6 +63,8 @@ private:
   Int_t fneve;
   Int_t fcounter_frag;//
   Int_t fcounter_neut;//
+  Int_t fcounter;//
+  Bool_t finit,finit_erelmax,finit_nbins;//
   ClassDef(TArtEasyEventMixing,1);
 };
 #endif
